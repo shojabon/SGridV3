5ded6fc523a763d37a6c1cd71c2035310a0ac21b
shojabon <shojabon@gmail.com>
2020-12-16 10:23:35 +0900

    added SResponse updated docker endpoint

--- SGridNode/Endpoints/DockerEndpoint.py
+++ SGridNode/Endpoints/DockerEndpoint.py
@@ -1,5 +1,6 @@
 import traceback
 
+from API.SResponse import SResponse
 from starlette.responses import JSONResponse
 
 from SGridNode.NodeMain import SGridV3Node
@@ -18,121 +19,119 @@ class DockerEndpoint:
         async def container_list(request: Request):
             json = await request.json()
             if not self.core.tool_function.does_post_params_exist(json, ["master_key", "all", "name_only"]):
-                return JSONResponse({"body": "Not Enough Params", "code": "params.not_enough"}, 400)
+                return SResponse("params.lacking").web()
             if self.core.config["master_key"] != json["master_key"]:
-                return JSONResponse({"body": "Credentials Invalid", "code": "credentials.invalid"}, 401)
+                return SResponse("key.invalid").web()
             if type(json["all"]) != bool:
-                return JSONResponse({"body": "Not Enough Params", "code": "params.not_enough"}, 400)
+                return SResponse("params.lacking").web()
             if json["name_only"]:
                 lis = [json_class.loads(json_class.dumps(x.__dict__, default=lambda o: '<not serializable>'))["attrs"]["Name"] for x in
                        self.core.docker.containers.list(json["all"])]
             else:
                 lis = [json_class.loads(json_class.dumps(x.__dict__, default=lambda o: '<not serializable>')) for x in
                        self.core.docker.containers.list(json["all"])]
-            return JSONResponse({"body": lis, "code": "Success"}, 200)
+            return SResponse("success", lis).web()
 
         @self.core.fast_api.route("/docker/container/stats", methods=["POST"])
         async def container_stats(request: Request):
             json = await request.json()
             if not self.core.tool_function.does_post_params_exist(json, ["master_key"]):
-                return JSONResponse({"body": "Not Enough Params", "code": "params.not_enough"}, 400)
+                return SResponse("params.lacking").web()
             if self.core.config["master_key"] != json["master_key"]:
-                return JSONResponse({"body": "Credentials Invalid", "code": "credentials.invalid"}, 401)
-
+                return SResponse("key.invalid").web()
             try:
                 result = []
                 for container in self.core.docker.containers.list():
                     result.append(container.stats(stream=False))
-                return JSONResponse({"body": result, "code": "Success"}, 200)
+                return SResponse("success", result).web()
             except Exception:
-                return JSONResponse({"body": traceback.format_exc(), "code": "error.internal"}, 500)
+                return SResponse("internal.error").web()
 
         @self.core.fast_api.route("/docker/container/run", methods=["POST"])
         async def container_run(request: Request):
             json = await request.json()
             if not self.core.tool_function.does_post_params_exist(json, ["master_key", "image"]):
-                return JSONResponse({"body": "Not Enough Params", "code": "params.not_enough"}, 400)
+                return SResponse("params.lacking").web()
             if self.core.config["master_key"] != json["master_key"]:
-                return JSONResponse({"body": "Credentials Invalid", "code": "credentials.invalid"}, 401)
+                return SResponse("key.invalid").web()
             del json["master_key"]
             try:
                 self.core.docker.containers.run(**json)
-                return JSONResponse({"body": "", "code": "Success"}, 200)
+                return SResponse("success").web()
             except Exception:
-                return JSONResponse({"body": traceback.format_exc(), "code": "error.internal"}, 500)
+                return SResponse("internal.error").web()
 
         @self.core.fast_api.route("/docker/container/stop", methods=["POST"])
         async def container_stop(request: Request):
             json = await request.json()
             if not self.core.tool_function.does_post_params_exist(json, ["master_key", "id"]):
-                return JSONResponse({"body": "Not Enough Params", "code": "params.not_enough"}, 400)
+                return SResponse("params.lacking").web()
             if self.core.config["master_key"] != json["master_key"]:
-                return JSONResponse({"body": "Credentials Invalid", "code": "credentials.invalid"}, 401)
-
+                return SResponse("key.invalid").web()
             try:
                 self.core.docker.containers.get(json["id"]).stop()
-                return JSONResponse({"body": "", "code": "Success"}, 200)
+                return SResponse("success").web()
             except Exception:
-                return JSONResponse({"body": traceback.format_exc(), "code": "error.internal"}, 500)
+                return SResponse("internal.error").web()
 
         @self.core.fast_api.route("/docker/container/start", methods=["POST"])
         async def container_start(request: Request):
             json = await request.json()
             if not self.core.tool_function.does_post_params_exist(json, ["master_key", "id"]):
-                return JSONResponse({"body": "Not Enough Params", "code": "params.not_enough"}, 400)
+                return SResponse("params.lacking").web()
             if self.core.config["master_key"] != json["master_key"]:
-                return JSONResponse({"body": "Credentials Invalid", "code": "credentials.invalid"}, 401)
+                return SResponse("key.invalid").web()
 
             try:
                 self.core.docker.containers.get(json["id"]).start()
                 return JSONResponse({"body": "", "code": "Success"}, 200)
             except Exception:
-                return JSONResponse({"body": traceback.format_exc(), "code": "error.internal"}, 500)
+                return SResponse("internal.error").web()
 
         @self.core.fast_api.route("/docker/image/list", methods=["POST"])
         async def image_list(request: Request):
             json = await request.json()
             if not self.core.tool_function.does_post_params_exist(json, ["master_key", "all"]):
-                return JSONResponse({"body": "Not Enough Params", "code": "params.not_enough"}, 400)
+                return SResponse("params.lacking").web()
             if self.core.config["master_key"] != json["master_key"]:
-                return JSONResponse({"body": "Credentials Invalid", "code": "credentials.invalid"}, 401)
+                return SResponse("key.invalid").web()
             if type(json["all"]) != bool:
-                return JSONResponse({"body": "Not Enough Params", "code": "params.not_enough"}, 400)
+                return SResponse("params.lacking").web()
             lis = [json_class.loads(json_class.dumps(x.__dict__, default=lambda o: '<not serializable>')) for x in self.core.docker.images.list(all=json["all"])]
-            return JSONResponse({"body": lis, "code": "Success"}, 200)
+            return SResponse("success", lis).web()
 
         @self.core.fast_api.route("/docker/image/build", methods=["POST"])
         async def image_build(request: Request):
             json = await request.json()
             if not self.core.tool_function.does_post_params_exist(json, ["master_key"]):
-                return JSONResponse({"body": "Not Enough Params", "code": "params.not_enough"}, 400)
+                return SResponse("params.lacking").web()
             if self.core.config["master_key"] != json["master_key"]:
-                return JSONResponse({"body": "Credentials Invalid", "code": "credentials.invalid"}, 401)
+                return SResponse("key.invalid").web()
             self.core.docker_function.load_images_from_sync()
-            return JSONResponse({"body": "", "code": "Success"}, 200)
+            return SResponse("success").web()
 
         @self.core.fast_api.route("/docker/image/delete", methods=["POST"])
         async def image_delete(request: Request):
             json = await request.json()
             if not self.core.tool_function.does_post_params_exist(json, ["master_key", "image"]):
-                return JSONResponse({"body": "Not Enough Params", "code": "params.not_enough"}, 400)
+                return SResponse("params.lacking").web()
             if self.core.config["master_key"] != json["master_key"]:
-                return JSONResponse({"body": "Credentials Invalid", "code": "credentials.invalid"}, 401)
+                return SResponse("key.invalid").web()
             try:
                 self.core.docker.images.remove(json["image"])
             except Exception:
-                return JSONResponse({"body": "Image Delete Error", "code": "error.internal"}, 500)
-            return JSONResponse({"body": "", "code": "Success"}, 200)
+                return SResponse("internal.error").web()
+            return SResponse("success").web()
 
         @self.core.fast_api.route("/docker/container/execute", methods=["POST"])
         async def container_exec(request: Request):
             json = await request.json()
             if not self.core.tool_function.does_post_params_exist(json, ["master_key", "id", "command"]):
-                return JSONResponse({"body": "Not Enough Params", "code": "params.not_enough"}, 400)
+                return SResponse("params.lacking").web()
             if self.core.config["master_key"] != json["master_key"]:
-                return JSONResponse({"body": "Credentials Invalid", "code": "credentials.invalid"}, 401)
+                return SResponse("key.invalid").web()
             try:
                 result = self.core.docker.containers.get(json["id"]).exec_run(cmd=json["command"], tty=True)
-                return JSONResponse({"body": result.output.decode("utf-8"), "code": "Success"}, 200)
+                return SResponse("success", result.output.decode("utf-8")).web()
             except Exception:
-                return JSONResponse({"body": traceback.format_exc(), "code": "error.internal"}, 500)
+                return SResponse("internal.error").web()
