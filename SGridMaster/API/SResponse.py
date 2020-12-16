5b610d00bb9bbdfb2df2efb329fb13b4d232fe11
shojabon <shojabon@gmail.com>
2020-12-16 20:07:23 +0900

    updated docker endpoint

---
+++ SGridMaster/API/SResponse.py
@@ -0,0 +1,68 @@
+import json
+
+from starlette.responses import JSONResponse
+
+
+class SResponse:
+
+    def __init__(self, error_code: str = None, body=None):
+        self.error_code = {
+            "success": {"success": True},
+            "email.confirm.updated": {"success": True, "body": "Email Confirmation Key Updated"},
+            "account.invalid": {"body": "Account Is Invalid"},
+            "application.invalid": {"body": "Application Is Invalid"},
+            "account.exists": {"body": "Account Exists"},
+            "internal.error": {"body": "Internal Error"},
+            "account.email.unconfirmed": {"body": "Email Not Confirmed"},
+            "token.invalid": {"body": "Token Is Invalid"},
+            "token.expired": {"body": "Token Expired"},
+            "params.lacking": {"body": "Lacking Params"},
+            "email.invalid": {"body": "Email Is Invalid"},
+            "password.invalid": {"body": "Password Invalid"},
+            "email.unconfirmed": {"body": "Email Is Unconfirmed"},
+            "web.error": {"body": "Web Connection Error"},
+            "key.invalid": {"body": "Key Is Invalid"},
+            "message.invalid": {"body": "Message Is Invalid"},
+            "object.invalid": {"body": "Object Is Invalid"},
+            "task.exists": {"body": "Task Already Exists"},
+            "node.invalid": {"body": "Node Is Invalid"}
+        }
+        self.data = {"code": "unknown", "success": False}
+        if error_code in self.error_code:
+            self.data = self.error_code[error_code]
+            self.data["code"] = error_code
+            if body is not None:
+                self.data["body"] = body
+            if "success" not in self.data:
+                self.data["success"] = False
+        if error_code is None:
+            self.data = body
+
+    def __str__(self):
+        return json.dumps(self.data, default=lambda o: '<not serializable>')
+
+    def web(self):
+        return JSONResponse(json.loads(json.dumps(self.data, default=lambda o: '<not serializable>')))
+
+    def success(self):
+        if "success" in self.data:
+            return self.data["success"]
+        return False
+
+    def fail(self):
+        if "success" in self.data:
+            return not self.data["success"]
+        return True
+
+    def code(self):
+        if "code" in self.data:
+            return self.data["code"]
+        return None
+
+    def body(self):
+        if "body" in self.data:
+            return self.data["body"]
+        return None
+
+    def serialize(self):
+        return self.data
--- SGridMaster/Endpoints/DockerEndpoint.py
+++ SGridMaster/Endpoints/DockerEndpoint.py
@@ -1,6 +1,7 @@
 import traceback
 from datetime import datetime
 
+from API.SResponse import SResponse
 from starlette.responses import JSONResponse
 
 from MasterMain import SGridV3Master
@@ -20,96 +21,83 @@ class DockerEndpoint:
         async def container_run(request: Request):
             json = await request.json()
             if not self.core.tool_function.does_post_params_exist(json, ["master_key", "node", "image"]):
-                return JSONResponse({"body": "Not Enough Params", "code": "params.not_enough"}, 400)
+                return SResponse("params.lacking").web()
             if self.core.config["master_key"] != json["master_key"]:
-                return JSONResponse({"body": "Credentials Invalid", "code": "credentials.invalid"}, 401)
+                return SResponse("key.invalid").web()
             if not self.core.tool_function.is_node(json["node"]):
-                return JSONResponse({"body": "Node Not Valid", "code": "error.internal"}, 500)
+                return SResponse("node.invalid").web()
             try:
                 sgrid = self.core.tool_function.get_sgrid_node(json["node"])
                 del json["node"]
-                result = sgrid.container_run(**json)
-                if not result:
-                    return JSONResponse({"body": "Internal Error", "code": "error.internal"}, 500)
+                return sgrid.container_run(**json).web()
             except Exception:
-                return JSONResponse({"body": "Internal Error", "code": "error.internal"}, 500)
-            return JSONResponse({"body": "", "code": "Success"}, 200)
+                return SResponse("internal.error").web()
 
         @self.core.fast_api.route("/docker/container/stop", methods=["POST"])
         async def container_stop(request: Request):
             json = await request.json()
             if not self.core.tool_function.does_post_params_exist(json, ["master_key", "node", "container"]):
-                return JSONResponse({"body": "Not Enough Params", "code": "params.not_enough"}, 400)
+                return SResponse("params.lacking").web()
             if self.core.config["master_key"] != json["master_key"]:
-                return JSONResponse({"body": "Credentials Invalid", "code": "credentials.invalid"}, 401)
+                return SResponse("key.invalid").web()
             if not self.core.tool_function.is_node(json["node"]):
-                return JSONResponse({"body": "Node Not Valid", "code": "error.internal"}, 500)
+                return SResponse("node.invalid").web()
             try:
                 sgrid = self.core.tool_function.get_sgrid_node(json["node"])
-                result = sgrid.container_stop(json["container"])
-                if not result:
-                    return JSONResponse({"body": "Internal Error", "code": "error.internal"}, 500)
+                return sgrid.container_stop(json["container"]).web()
             except Exception:
-                return JSONResponse({"body": "Internal Error", "code": "error.internal"}, 500)
-            return JSONResponse({"body": "", "code": "Success"}, 200)
+                return SResponse("internal.error").web()
 
         @self.core.fast_api.route("/docker/container/start", methods=["POST"])
         async def container_start(request: Request):
             json = await request.json()
             if not self.core.tool_function.does_post_params_exist(json, ["master_key", "node", "container"]):
-                return JSONResponse({"body": "Not Enough Params", "code": "params.not_enough"}, 400)
+                return SResponse("params.lacking").web()
             if self.core.config["master_key"] != json["master_key"]:
-                return JSONResponse({"body": "Credentials Invalid", "code": "credentials.invalid"}, 401)
+                return SResponse("key.invalid").web()
             if not self.core.tool_function.is_node(json["node"]):
-                return JSONResponse({"body": "Node Not Valid", "code": "error.internal"}, 500)
+                return SResponse("node.invalid").web()
             try:
                 sgrid = self.core.tool_function.get_sgrid_node(json["node"])
-                result = sgrid.container_start(json["container"])
-                if not result:
-                    return JSONResponse({"body": "Internal Error", "code": "error.internal"}, 500)
+                return sgrid.container_start(json["container"]).web()
             except Exception:
-                return JSONResponse({"body": "Internal Error", "code": "error.internal"}, 500)
-            return JSONResponse({"body": "", "code": "Success"}, 200)
+                return SResponse("internal.error").web()
 
         @self.core.fast_api.route("/docker/container/list", methods=["POST"])
         async def container_list(request: Request):
             json = await request.json()
             if not self.core.tool_function.does_post_params_exist(json, ["master_key", "node", "name_only"]):
-                return JSONResponse({"body": "Not Enough Params", "code": "params.not_enough"}, 400)
+                return SResponse("params.lacking").web()
             if self.core.config["master_key"] != json["master_key"]:
-                return JSONResponse({"body": "Credentials Invalid", "code": "credentials.invalid"}, 401)
+                return SResponse("key.invalid").web()
             if not self.core.tool_function.is_node(json["node"]):
-                return JSONResponse({"body": "Node Not Valid", "code": "error.internal"}, 500)
-
+                return SResponse("node.invalid").web()
             node = json["node"]
             if node in self.container_list_time_cache.keys() and node in self.container_list_cache.keys() and datetime.now().timestamp() - self.container_list_time_cache[node] < 1:
-                return JSONResponse({"body": self.container_list_cache[node], "code": "Success"}, 200)
+                return SResponse("success", self.container_list_cache[node]).web()
 
             try:
                 sgrid = self.core.tool_function.get_sgrid_node(json["node"])
                 result = sgrid.container_list(name_only=json["name_only"])
-                if result is None:
-                    return JSONResponse({"body": "Internal Error", "code": "error.internal"}, 500)
+                if result.fail():
+                    return result.web()
                 self.container_list_cache[node] = result
                 self.container_list_time_cache[node] = datetime.now().timestamp()
+                return result.web()
             except Exception:
-                return JSONResponse({"body": "Internal Error", "code": "error.internal"}, 500)
-            return JSONResponse({"body": result, "code": "Success"}, 200)
+                return SResponse("internal.error").web()
 
         @self.core.fast_api.route("/docker/container/execute", methods=["POST"])
         async def container_start(request: Request):
             json = await request.json()
             if not self.core.tool_function.does_post_params_exist(json, ["master_key", "node", "container", "command"]):
-                return JSONResponse({"body": "Not Enough Params", "code": "params.not_enough"}, 400)
+                return SResponse("params.lacking").web()
             if self.core.config["master_key"] != json["master_key"]:
-                return JSONResponse({"body": "Credentials Invalid", "code": "credentials.invalid"}, 401)
+                return SResponse("key.invalid").web()
             if not self.core.tool_function.is_node(json["node"]):
-                return JSONResponse({"body": "Node Not Valid", "code": "error.internal"}, 500)
+                return SResponse("node.invalid").web()
             try:
                 sgrid = self.core.tool_function.get_sgrid_node(json["node"])
-                result = sgrid.container_exec(json["container"], json["command"])
-                if result is None:
-                    return JSONResponse({"body": "Internal Error", "code": "error.internal"}, 500)
-                return JSONResponse({"body": result, "code": "Success"}, 200)
+                return sgrid.container_exec(json["container"], json["command"]).web()
             except Exception:
-                return JSONResponse({"body": "Internal Error", "code": "error.internal"}, 500)
+                return SResponse("internal.error").web()
